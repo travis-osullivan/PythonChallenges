fae7800994913c1e775a81721b9860bcd86978ad
Travis O'Sullivan <tsullyo@gmail.com>
2017-08-16 22:09:37 -0400

    Cleaned up Challenge 3 with pythonic upsert

--- PythonChallenges/PythonChallenge.3.py
+++ PythonChallenges/PythonChallenge.3.py
@@ -4,12 +4,12 @@ INPUT_DATA = Utils.GetChallengeInput(3)
 
 COUNT_DICT = {}
 for elt in INPUT_DATA:
-    if elt not in COUNT_DICT:
-        COUNT_DICT[elt] = 1
-    else:
-        COUNT_DICT[elt] = COUNT_DICT[elt] + 1
+    # Fancy, Pythonic upsert. Returns count, if key exists, or defaults to 0 if key not found
+    # Then increments in either case
+    COUNT_DICT[elt] = COUNT_DICT.setdefault(elt, 0) + 1
 
 ANSWER = ""
+# TODO: clean this up as well. Dictionary Comprehensions look promising
 for entry in COUNT_DICT:
     print("{0}: {1}".format(entry, COUNT_DICT[entry]))
     if COUNT_DICT[entry] == 1:
